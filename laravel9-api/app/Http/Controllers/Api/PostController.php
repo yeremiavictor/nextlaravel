6a008a72b3a51f15adb32116e0e8e2163260cb65
yeremiavictor <yeremiavictor@gmail.com>
2023-05-09 13:33:45 +0700

    init create

--- laravel9-api/app/Http/Controllers/Api/PostController.php
+++ laravel9-api/app/Http/Controllers/Api/PostController.php
@@ -7,8 +7,12 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 
+//dipakai untuk post
+use Illuminate\Support\Facades\Validator;
+
 class PostController extends Controller
 {
+    //get
     /**
      * index
      * 
@@ -21,4 +25,40 @@ class PostController extends Controller
         //return collection of posts as a resource
         return new PostResource(true, 'List Data Posts', $posts);
     }
+
+    //store(add)
+    /**
+     * store
+     * 
+     * @param mixed $request
+     * @return void
+     */
+    public function store(Request $request){
+        //mendefinisakn peraturan validasi
+        $validator = Validator::make($request->all(),[
+            'image' => 'required|image|mimes:jpeg,png,gif,svg|max:2048',
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        //cek kalau validasi gagal
+        if($validator->fails()){
+            return response()->json($validator->errors(),442);
+        }
+
+        //upload gambar
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //unggah post ke database
+        $post = Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content,
+        ]);
+
+        //respon setelah berhasil
+        return new PostResource(true, 'Data berhasil dibuat', $post);
+    }
+
 }
