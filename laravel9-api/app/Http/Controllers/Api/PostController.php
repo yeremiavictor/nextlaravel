2e02960544eeeb876b6cf48476a99c16c6d1074b
yeremiavictor <yeremiavictor@gmail.com>
2023-05-09 13:49:23 +0700

    init detail

--- laravel9-api/app/Http/Controllers/Api/PostController.php
+++ laravel9-api/app/Http/Controllers/Api/PostController.php
@@ -12,53 +12,68 @@ use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    //get
-    /**
-     * index
-     * 
-     * @return void
-     */
-    public function index() {
-        //get posts
-        $posts = Post::latest()->paginate(5);
 
-        //return collection of posts as a resource
-        return new PostResource(true, 'List Data Posts', $posts);
-    }
+    //get
+        /**
+         * index
+         * 
+         * @return void
+         */
+        public function index() {
+            //get posts
+            $posts = Post::latest()->paginate(5);
+
+            //return collection of posts as a resource
+            return new PostResource(true, 'List Data Posts', $posts);
+        }
+    // get
 
     //store(add)
-    /**
-     * store
-     * 
-     * @param mixed $request
-     * @return void
-     */
-    public function store(Request $request){
-        //mendefinisakn peraturan validasi
-        $validator = Validator::make($request->all(),[
-            'image' => 'required|image|mimes:jpeg,png,gif,svg|max:2048',
-            'title' => 'required',
-            'content' => 'required',
-        ]);
+        /**
+         * store
+         * 
+         * @param mixed $request
+         * @return void
+         */
+        public function store(Request $request){
+            //mendefinisakn peraturan validasi
+            $validator = Validator::make($request->all(),[
+                'image' => 'required|image|mimes:jpeg,png,gif,svg|max:2048',
+                'title' => 'required',
+                'content' => 'required',
+            ]);
 
-        //cek kalau validasi gagal
-        if($validator->fails()){
-            return response()->json($validator->errors(),442);
+            //cek kalau validasi gagal
+            if($validator->fails()){
+                return response()->json($validator->errors(),442);
+            }
+
+            //upload gambar
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //unggah post ke database
+            $post = Post::create([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+
+            //respon setelah berhasil
+            return new PostResource(true, 'Data berhasil dibuat', $post);
         }
+    //add
 
-        //upload gambar
-        $image = $request->file('image');
-        $image->storeAs('public/posts', $image->hashName());
-
-        //unggah post ke database
-        $post = Post::create([
-            'image'     => $image->hashName(),
-            'title'     => $request->title,
-            'content'   => $request->content,
-        ]);
-
-        //respon setelah berhasil
-        return new PostResource(true, 'Data berhasil dibuat', $post);
-    }
-
+    //detail
+        /**
+         * show
+         * 
+         * @param mixed $post
+         * @return void
+         */
+        public function show(Post $post){
+            //menampilkan sebuah post dari sumber data
+            return new PostResource(true, 'Data ditemukan', $post);
+        }
+    //detail
 }
